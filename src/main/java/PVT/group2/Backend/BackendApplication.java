c610fe4046915965b177d5e24fa565a10c708ca5
zachath <zachariasthorell@gmail.com>
2021-04-20 11:15:13 +0200

    setup for jenkins

--- src/main/java/PVT/group2/Backend/BackendApplication.java
+++ src/main/java/PVT/group2/Backend/BackendApplication.java
@@ -2,12 +2,22 @@ package PVT.group2.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class BackendApplication {
+@RestController
+public class BackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s", name);
 	}
 
 }
