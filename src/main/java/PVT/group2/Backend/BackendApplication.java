8afb12032754d84497327bcf84db1acebc09fe1d
simon <simon.eklundh@gmail.com>
2021-04-22 10:45:54 +0200

    added and tested mathemathic method

--- src/main/java/PVT/group2/Backend/BackendApplication.java
+++ src/main/java/PVT/group2/Backend/BackendApplication.java
@@ -11,13 +11,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
-	}
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
+    }
+
+    @GetMapping("/maths")
+    public int calculate(@RequestParam(value = "left", defaultValue = "0") int left,
+                         @RequestParam(value = "right", defaultValue = "0") int right,
+                         @RequestParam(value = "operator", defaultValue = "+") char operator) throws ArithmeticException {
+
+        switch (operator) {
+            case '-':
+                return left - right;
+            case '*':
+                return left * right;
+            case '/':
+                if (right == 0) throw new ArithmeticException("you can't divide by zero");
+                return left / right;
+            default:
+                return left + right;
+        }
+
+
+    }
 
 }
--- src/test/java/PVT/group2/Backend/BackendApplicationTests.java
+++ src/test/java/PVT/group2/Backend/BackendApplicationTests.java
@@ -4,20 +4,39 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	void testHello() {
-		String out = new BackendApplication().hello("Peter");
+    @Test
+    void testHelloReturnsCorrectInput() {
+        assertEquals("Hello Peter", new BackendApplication().hello("Peter"), "Error in Hello with strings");
+    }
 
-		assertEquals("Hello Peter", out, "Error in Hello");
-	}
+    @Test
+    void testHelloWitNumbers() {
+        assertEquals("Hello 14", new BackendApplication().hello("14"), "Error in hello with numbers");
+    }
 
+    @Test
+    void testCalculateBasicCalculations(){
+        assertEquals(5, new BackendApplication().calculate(2,3, '+'), "failed plus calculation");
+        assertEquals(2, new BackendApplication().calculate(5,3,'-'), "failed subtraction calculation");
+        assertEquals(2, new BackendApplication().calculate(4,2,'/'), "failed division calculation");
+        assertEquals(9, new BackendApplication().calculate(3,3,'*'), "failed multiplication calculation");
+    }
+
+    @Test
+    void testCalculateThrowsExceptionOnDivisionByZero(){
+        BackendApplication test = new BackendApplication();
+       assertThrows(ArithmeticException.class, () -> test.calculate(5,0, '/'), "you can't divide by 0");
+
+
+    }
 }
