e9409e98841717c918a57c5d60008dd2416568b0
Simon Eklundh <simon.eklundh@gmail.com>
2021-05-05 20:37:13 +0200

    added a comment to test push to jenkins

--- src/main/java/PVT/group2/Backend/BackendApplication.java
+++ src/main/java/PVT/group2/Backend/BackendApplication.java
@@ -14,7 +14,7 @@ public class BackendApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
+    //basic hello world
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s", name);
