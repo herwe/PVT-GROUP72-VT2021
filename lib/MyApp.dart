e187aa0d7077bd58614f0cb082fb235e0c891a43
Simon Eklundh <simon.eklundh@gmail.com>
2021-05-07 14:37:33 +0200

    added changing routes

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -253,21 +253,112 @@ class ListRoute extends StatefulWidget {
 }
 
 class _ListRouteState extends State<ListRoute> {
-  _MyAppState test = new _MyAppState();
+  AppBar buildAppBar() {
+    return AppBar(
+      toolbarHeight: 48,
+      bottom: TabBar(
+        controller: _tabController,
+        tabs: <Widget>[
+          Tab(
+            icon: Icon(Icons.map),
+          ),
+          Tab(
+            icon: Icon(Icons.filter_alt_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column buildFloatingActionButtonsColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        FloatingActionButton(
+          // onPressed: _currentLocation,
+          child: Icon(Icons.search),
+        ),
+        FloatingActionButton(
+          //    onPressed: _currentLocation,
+          child: Icon(Icons.filter_alt_outlined),
+        ),
+        FloatingActionButton(
+          // onPressed: _currentLocation,
+          child: Icon(Icons.location_on),
+        ),
+        FloatingActionButton(
+          //     onPressed: loadToilets,
+          child: Icon(Icons.airline_seat_legroom_extra),
+        ),
+        /**  FloatingActionButton(
+            onPressed: goBack,
+            child: Icon(Icons.home),
+            ),**/
+      ],
+    );
+  }
+
+  Widget buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        appBar: test.buildAppBar(),
+        appBar: buildAppBar(),
         body: Stack(
           fit: StackFit.expand,
           children: [
-            test.buildGoogleMap(),
-            test.buildFloatingSearchBar(),
+            buildFloatingSearchBar(),
           ],
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: test.buildFloatingActionButtonsColumn());
+        floatingActionButton: buildFloatingActionButtonsColumn());
   }
 }
