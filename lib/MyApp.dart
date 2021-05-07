f0152b78c3caa50d3b0ac20da39668de41204a10
Simon Eklundh <simon.eklundh@gmail.com>
2021-05-07 13:41:21 +0200

    some more refactoring

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -153,68 +153,83 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   }
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-      home: Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-            toolbarHeight: 48,
-            bottom: TabBar(
-              controller: _tabController,
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.map),
-                ),
-                Tab(
-                  icon: Icon(Icons.filter_alt_outlined),
-                ),
-              ],
-            ),
-          ),
-          body: Stack(
-            fit: StackFit.expand,
-            children: [
-              GoogleMap(
-                mapToolbarEnabled: false,
-                onMapCreated: _onMapCreated,
-                mapType: MapType.normal,
-                markers: Set<Marker>.of(markers.values),
-                zoomControlsEnabled: false,
-                myLocationEnabled: true,
-                myLocationButtonEnabled: false,
-                initialCameraPosition: CameraPosition(
-                  target: _center,
-                  zoom: 10,
-                ),
-              ),
-              buildFloatingSearchBar(),
-            ],
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-          floatingActionButton: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              FloatingActionButton(
-                onPressed: _currentLocation,
-                child: Icon(Icons.search),
-              ),
-              FloatingActionButton(
-                onPressed: _currentLocation,
-                child: Icon(Icons.filter_alt_outlined),
-              ),
-              FloatingActionButton(
-                onPressed: _currentLocation,
-                child: Icon(Icons.location_on),
-              ),
-              FloatingActionButton(
-                onPressed: loadToilets,
-                child: Icon(Icons.airline_seat_legroom_extra),
-              ),
-              /**  FloatingActionButton(
-                      onPressed: goBack,
-                      child: Icon(Icons.home),
-                      ),**/
-            ],
-          ))
+  Widget build(BuildContext context) => MaterialApp(home: buildScaffold()
       //mainAxisAlignment: MainAxisAlignment.spaceBetween
       );
+
+  Scaffold buildScaffold() {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: buildAppBar(),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            buildGoogleMap(),
+            buildFloatingSearchBar(),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: buildFloatingActionButtonsColumn());
+  }
+
+  Column buildFloatingActionButtonsColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        FloatingActionButton(
+          onPressed: _currentLocation,
+          child: Icon(Icons.search),
+        ),
+        FloatingActionButton(
+          onPressed: _currentLocation,
+          child: Icon(Icons.filter_alt_outlined),
+        ),
+        FloatingActionButton(
+          onPressed: _currentLocation,
+          child: Icon(Icons.location_on),
+        ),
+        FloatingActionButton(
+          onPressed: loadToilets,
+          child: Icon(Icons.airline_seat_legroom_extra),
+        ),
+        /**  FloatingActionButton(
+                  onPressed: goBack,
+                  child: Icon(Icons.home),
+                  ),**/
+      ],
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      toolbarHeight: 48,
+      bottom: TabBar(
+        controller: _tabController,
+        tabs: <Widget>[
+          Tab(
+            icon: Icon(Icons.map),
+          ),
+          Tab(
+            icon: Icon(Icons.filter_alt_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+
+  GoogleMap buildGoogleMap() {
+    return GoogleMap(
+      mapToolbarEnabled: false,
+      onMapCreated: _onMapCreated,
+      mapType: MapType.normal,
+      markers: Set<Marker>.of(markers.values),
+      zoomControlsEnabled: false,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 10,
+      ),
+    );
+  }
 }
