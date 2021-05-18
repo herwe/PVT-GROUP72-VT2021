61ce70300fb5b364f01f26f31b7b1286181845d5
Simon Eklundh <simon.eklundh@gmail.com>
2021-05-18 12:28:58 +0200

    returned to using floatingsearchbar

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -6,10 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/qualities.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 import 'filterSheet.dart';
 import 'park.dart';
@@ -217,28 +217,49 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   Widget buildFloatingSearchBar() {
     final isPortrait =
         MediaQuery.of(context).orientation == Orientation.portrait;
-    return TypeAheadField(
-      textFieldConfiguration: TextFieldConfiguration(
-          autofocus: true,
-          style: DefaultTextStyle.of(context)
-              .style
-              .copyWith(fontStyle: FontStyle.italic),
-          decoration: InputDecoration(border: OutlineInputBorder())),
-      suggestionsCallback: (pattern) async {
-        return await getSuggestions(pattern);
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        getSuggestions(query);
+        // Call your model, bloc, controller here.
       },
-      itemBuilder: (context, suggestion) {
-        return ListTile(
-          leading: Icon(Icons.shopping_cart),
-          title: Text(suggestion['name']),
-          subtitle: Text('\$${suggestion['price']}'),
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
         );
-      },
-      onSuggestionSelected: (suggestion) {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: null,
-          //  builder: (context) => ProductPage(product: suggestion)
-        ));
       },
     );
   }
